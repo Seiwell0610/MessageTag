076f25fa42e5df9a695890ca6a2171492603b34d
Seiwell <M.Kousei.0610@gmail.com>
2021-03-22 16:18:21 +0900

    初版

---
+++ cogs/SetTags.py
@@ -0,0 +1,112 @@
+import discord
+from discord.ext import commands
+import os
+import sqlite3 as sq3
+import re
+
+print(f"{os.path.basename(__file__)}")
+
+# データベースに接続
+connect = sq3.connect("all_data.db")
+cursor = connect.cursor()
+
+
+class SetTags(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def tag(self, ctx, tag_name: str = None):
+        try:
+            # タグを指定していない場合
+            if tag_name is None:
+                return await ctx.send(f"{ctx.author.mention}-> タグを指定してください。")
+
+            # 該当のタグが付いているデータを取得
+            data = cursor.execute(f'SELECT * FROM "{ctx.author.id}" WHERE tag_name=?', (tag_name, )).fetchall()
+
+            # 該当のタグが付いているデータがない場合
+            if data is None:
+                return await ctx.send(f"{ctx.author.mention}-> 該当のタグの付いているデータはありませんでした。")
+
+            # あった場合
+            embed = discord.Embed(title=f"**{tag_name}**の付いているタグを{len(data)}件見つかりました。", color=discord.Color.green())
+            for row in data:
+                embed.add_field(name=f"{row[1]}", value=f"[{row[2][:35]}...]({row[2]})", inline=False)
+            await ctx.send(embed=embed)
+
+        except sq3.OperationalError:
+            cursor.execute(f'CREATE TABLE "{ctx.author.id}"'
+                           f'(Tag_name TEXT NOT NULL, Unique_Name TEXT NOT NULL UNIQUE, Message_Link TEXT NOT NULL)')
+            await ctx.send(f"{ctx.author.mention}-> DBを作成しました。再度コマンドを実行してみて下さい。")
+
+    @commands.command()
+    async def add(self, ctx, tag_name: str = None, unique_name: str = None, value: str = None):
+        # 正規表現
+        regex_discord_message_url = (
+            'https://(ptb.|canary.)?discord(app)?.com/channels/'
+            '(?P<guild>[0-9]{18})/(?P<channel>[0-9]{18})/(?P<message>[0-9]{18})'
+        )
+
+        try:
+            if tag_name is None:
+                return await ctx.send(f"{ctx.author.mention}-> タグを指定してください。")
+
+            if unique_name is None:
+                return await ctx.send(f"{ctx.author.mention}-> タイトルを指定してください。")
+
+            if value is None:
+                return await ctx.send(f"{ctx.author.mention}-> データを指定してください。")
+
+            if re.fullmatch(regex_discord_message_url, value) is None:
+                return await ctx.send(f"{ctx.author.mention}-> メッセージリンクを指定してください。")
+
+            if cursor.execute(f'SELECT * FROM "{ctx.author.id}" WHERE Unique_Name = ?', (unique_name, )).fetchone() is not None:
+                return await ctx.send(f"{ctx.author.mention}-> タイトルが重複しているため保存できません。")
+
+            cursor.execute(f'INSERT INTO "{ctx.author.id}" VALUES (?, ?, ?)', (tag_name, unique_name, value))
+            connect.commit()
+            return await ctx.send(f"{ctx.author.mention}-> 該当のデータを追加しました。")
+
+        except sq3.OperationalError:  # ユーザーのテーブルが存在しない場合、自動的に作成
+            cursor.execute(f'CREATE TABLE "{ctx.author.id}"'
+                           f'(Tag_name TEXT NOT NULL, Unique_Name TEXT NOT NULL UNIQUE, Message_Link TEXT NOT NULL)')
+            await ctx.send(f"{ctx.author.mention}-> DBを作成しました。再度コマンドを実行してみて下さい。")
+
+
+    @commands.command()
+    async def remove(self, ctx, tag_name: str = None, unique_name: str = None):
+        try:
+            if tag_name is None:
+                return await ctx.send(f"{ctx.author.mention}-> タグを指定してください。")
+
+            if unique_name is None:
+                return await ctx.send(f"{ctx.author.mention}-> タイトルを指定してください。")
+
+            data = cursor.execute(f'SELECT * FROM "{ctx.author.id}" WHERE Tag_Name = ? and Unique_Name = ?', (tag_name, unique_name, )).fetchone()
+
+            if data is None:
+                return await ctx.send(f"{ctx.author.mention}-> 該当のデータがありません。")
+
+            cursor.execute(f'DELETE FROM "{ctx.author.id}" WHERE Tag_Name = ? and Unique_Name = ?', (tag_name, unique_name))
+            connect.commit()
+            return await ctx.send(f"{ctx.author.mention}-> 該当のデータを削除しました。")
+
+        except sq3.OperationalError:
+            cursor.execute(f'CREATE TABLE "{ctx.author.id}"'
+                           f'(Tag_name TEXT NOT NULL, Unique_Name TEXT NOT NULL UNIQUE, Message_Link TEXT NOT NULL)')
+            await ctx.send(f"{ctx.author.mention}-> DBを作成しました。再度コマンドを実行してみて下さい。")
+
+    @commands.command()
+    async def ac(self, ctx):
+        try:
+            cursor.execute(f'SELECT * FROM "{ctx.author.id}"')
+
+        except sq3.OperationalError:
+            cursor.execute(f'CREATE TABLE "{ctx.author.id}"'
+                           f'(Tag_name TEXT NOT NULL, Unique_Name TEXT NOT NULL UNIQUE, Message_Link TEXT NOT NULL)')
+            await ctx.send(f"{ctx.author.mention}-> DBを作成しました。")
+
+
+def setup(bot):
+    bot.add_cog(SetTags(bot))
---
+++ main.py
@@ -0,0 +1,45 @@
+import discord
+import asyncio
+from discord.ext import commands
+import os
+import traceback
+
+token = os.environ["TOKEN"]
+prefix = "#"
+loop = asyncio.new_event_loop()
+
+
+async def run():
+    bot = MyBot()
+    try:
+        await bot.start(token)
+    except KeyboardInterrupt:
+        await bot.logout()
+
+
+class MyBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=commands.when_mentioned_or(prefix), loop=loop, intents=discord.Intents.all())
+        self.remove_command('help')
+
+    async def on_ready(self):
+        path = "./cogs"
+        for cog in os.listdir(path):
+            if cog.endswith(".py"):
+                try:
+                    self.load_extension(f"cogs.{cog[:-3]}")
+                except commands.ExtensionAlreadyLoaded:
+                    self.reload_extension(f"cogs.{cog[:-3]}")
+
+
+if __name__ == '__main__':
+    try:
+        print(f"Discord.pyのバージョン：{discord.__version__}")
+        print("起動完了")
+
+        main_task = loop.create_task(run())
+        loop.run_until_complete(main_task)
+        loop.close()
+
+    except Exception as error:
+        print("エラー情報\n" + traceback.format_exc())
