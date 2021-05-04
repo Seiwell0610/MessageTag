e6a136256a4d2777116e3d4b17e078dbc20ab499
Seiwell <seiwell0610@gmail.com>
2021-05-04 09:34:12 +0900

    ヘルプの修正

--- cogs/Help.py
+++ cogs/Help.py
@@ -13,9 +13,10 @@ class Help(commands.Cog):
     async def help(self, ctx):
         embed = discord.Embed(title="ヘルプ", color=discord.Color.blue())
 
-        embed.add_field(name="#tag <タグ名>", value="該当のタグが付いているデータを表示します。", inline=False)
-        embed.add_field(name="#add <タグ名> <タイトル> <メッセージリンク>", value="データを追加します。", inline=False)
-        embed.add_field(name="#remove <タグ名> <タイトル>", value="データを削除します。", inline=False)
+        embed.add_field(name="#tag", value="該当のタグが付いているデータを表示します。", inline=False)
+        embed.add_field(name="#add", value="データを追加します。", inline=False)
+        embed.add_field(name="#remove", value="データを削除します。", inline=False)
+        embed.add_field(name="#mytag", value="登録されているタグの一覧を表示", inline=False)
         embed.add_field(name="#ac", value="ユーザーのデータベースを作成します。", inline=False)
 
         await ctx.send(embed=embed)
